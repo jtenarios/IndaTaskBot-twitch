9885b8a6c559da630477def930fc3573897e3dc9
Jaime-Ryzen5 <kovenam@gmail.com>
2022-03-09 22:19:15 +0100

    Subir de nuevo version webpack para heroku y lectura del .env

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,68 @@
+// const path = require("path");
+// const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+// const Dotenv = require('dotenv-webpack');
+
+// module.exports = {
+//   entry: "./src/app.js",
+//   output: {
+//     filename: "bundle.js",
+//     path: path.resolve(__dirname, "dist"),
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: ["style-loader", "css-loader"],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HTMLWebpackPlugin({
+//       filename: "index.html",
+//       template: "./index.html",
+//     }),
+//     new Dotenv()
+//   ],
+//   mode: "production",
+// };
+
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
-
-const Dotenv = require('dotenv-webpack');
+const webpack = require("webpack"); // version Heroku
 
 module.exports = {
-  entry: "./src/app.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
+    entry: "./src/app.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"],
+        },],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: "index.html",
+            template: "./index.html",
+        }),
+        // version para subir a heroku // fuente: https://stackoverflow.com/questions/59759085/heroku-failed-to-load-env
+        new webpack.DefinePlugin({
+            'process.env': {
+                'USERNAME': JSON.stringify(process.env.USERNAME),
+                'PASSWORD': JSON.stringify(process.env.PASSWORD),
+                'CHANNELS': JSON.stringify(process.env.CHANNELS),
+                'CLIENT_ID': JSON.stringify(process.env.CLIENT_ID),
+                'CLIENT_SECRET': JSON.stringify(process.env.CLIENT_SECRET),
+            }
+        }),
     ],
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      filename: "index.html",
-      template: "./index.html",
-    }),
-    new Dotenv()
-  ],
-  mode: "production",
+    mode: "production",
+    performance: {
+
+        hints: false
+    }
+
 };
