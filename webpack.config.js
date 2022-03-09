7f4dfdd13ef42af7ade9e83d905feb0812c5d3dd
Jaime-Ryzen5 <kovenam@gmail.com>
2022-03-09 20:06:12 +0100

    importar jquery en el webapck config

--- webpack.config.js
+++ webpack.config.js
@@ -63,6 +63,11 @@ module.exports = {
     performance: {
 
         hints: false
+    },
+    externals: {
+      // require("jquery") is external and available
+      //  on the global var jQuery
+      "jquery": "jQuery"
     }
 
 };
